b7b5470d8ca6d9c4f4bc8b914176fc916fa5ace2
ggeorg29 <ggeorg29@uic.edu>
2021-12-01 15:23:23 -0600

    added a simple SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React from 'react';
+import "../assets/searchbar.css";
+import SearchIcon from '@mui/icons-material/Search';
 
 // Glieb
-export default function SearchBar() {
+export default function SearchBar({placeholder, data}) {
     return (
-        <div>
-            
+        <div className = "search">
+            <div className = "searchInputs">
+                <div className = "searchIcon">
+                    <SearchIcon/>
+                </div>
+                <input type= "text" placeholder = {placeholder}/>
+            </div>
+            <div className = "searchResult"></div>
         </div>
     )
 }
