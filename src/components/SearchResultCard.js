5afdf83035d2aee7b9e8ccae77baef187f2646d2
Jacob Diaz <hello@jacobdiaz.dev>
2021-12-01 16:22:25 -0600

    style: Fix result card style

--- src/components/SearchResultCard.js
+++ src/components/SearchResultCard.js
@@ -1,20 +1,23 @@
 import React from "react";
-import "../assets/searchcard.css"
+import "../assets/searchcard.css";
+import { Container } from "../components/StyledComponents/ReusableComponents";
 
-export default function SearchResultCard({title, paragraph, video}){
-    return(
-        <div className = "result">
-            <div className = "text-result">
-                <h2>{title}</h2>
-                <p>{paragraph}</p>
-                <div className = "buttons-result"> 
-                    <button className = "view">View Transcript</button>
-                    <button className = "read">Listen to Transcript</button>
-                </div>
-            </div>
-            <div className = "video-results">
-                <iframe src = {video}></iframe>
-            </div>
+export default function SearchResultCard({ title, paragraph, video }) {
+  return (
+    <Container>
+      <div className="result">
+        <div className="text-result">
+          <h2>{title}</h2>
+          <p className="paragraph">{paragraph}</p>
+          <div className="buttons-result">
+            <button className="viewBtn">View Transcript</button>
+            <button className="readBtn">Listen to Transcript</button>
+          </div>
         </div>
-    );
+        <div className="video-results">
+          <iframe src={video}></iframe>
+        </div>
+      </div>
+    </Container>
+  );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
-
+import SearchResultCard from "../components/SearchResultCard";
 export default class HomePage extends Component {
   render() {
     return (
       <div>
         <h1>Home Page</h1>
         {/* TODO Search Bar */}
-
+        <SearchResultCard
+          title="TItle"
+          paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+          video="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1"
+        />
         {/*Category Items*/}
         {/* TODO List of tutorial cards */}
       </div>
