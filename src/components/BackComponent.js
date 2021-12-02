3188278de713bfc77af8ea170d4b114010a05935
Jacob Diaz <hello@jacobdiaz.dev>
2021-12-01 21:30:03 -0600

    feat: Added Route to back component

--- src/components/BackComponent.js
+++ src/components/BackComponent.js
@@ -1,11 +1,16 @@
-import React from 'react'
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import "../assets/back.css"
+import React from "react";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import "../assets/back.css";
+import { Link } from "react-router-dom";
 
-export default function(){
-    return(
-        <div className = "back"> 
-            <h3><ArrowBackIcon className = "icon" /> Back To Home</h3>   
-        </div>
-    );
+export default function () {
+  return (
+    <Link to="/GR5/">
+      <div className="back">
+        <h3>
+          <ArrowBackIcon className="icon" /> Back To Home
+        </h3>
+      </div>
+    </Link>
+  );
 }
